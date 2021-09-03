8efede7d8abe2f77570e819816fe708a721e7df9
Fabien Riviere <fabien@activateurweb.com>
2021-09-03 10:33:07 +0200

    Mise en place de la structure du dossier (backend) et installation. Création du dossier frontend, avec structure pattern 7.1 pour Sass. Création des fichiers html page Accueil et template produit. Création feuilles de styles (scss) et création des fichiers JS, accueil et produit.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FabienRiviere_5_04092021
 Projet 5 formation DW chez OCR
+Réalisation d'un site e-commerce (Orinoco) avec page d'accueil, page produit, page panier et page de confirmation de commande.
+Utilisation de JavaScript pour les implantations, gestions, et intéractions des élements sur les pages.
---
+++ frontend/view/home/index.js
@@ -0,0 +1,33 @@
+// Affichage et interaction sur page d'acceuil
+
+ let url = `http://localhost:3000/api/cameras`;
+
+ fetch(url).then( response =>
+    response.json())
+    .then(jsonListCamera => {
+        for (let jsonCamera of jsonListCamera) {
+            let camera = new Camera(jsonCamera);
+            document.querySelector(".articles__container").innerHTML += `<div class="article__card">
+                                                                    <div class="article__card-info">
+                                                                        <img src='${camera.imageUrl}' class="article__card-img"/>                                                                  
+                                                                        <h3 id="name">${camera.name}</h3>
+                                                                        <p id="description">Description : ${camera.description}</p>
+                                                                        <p id="price">${camera.price} €</p>
+                                                                    </div>
+                                                                    <div class="personnalisation">
+                                                                        <div class="lenses">
+                                                                            <label for="lenses">Personnalisez avec nos lentilles :</label>
+                                                                            <select name="lentilles" id"lenses">
+                                                                                <option value="valeur1"selected>${camera.lenses[0]}</option>
+                                                                                <option value="valeur2">${camera.lenses[1]}</option>
+                                                                                <option value="valeur3">${camera.lenses[2]}</option>
+                                                                            </select>
+                                                                        </div>
+                                                                    </div>
+                                                                </div>`;
+        }
+        
+    
+    });
+
+    
