3b555178e0d22e09e1bce1c96254b293d620326b
Fabien Riviere <fabien@activateurweb.com>
2021-09-06 18:41:25 +0200

    Nouvelle méthode pour l'affichage des éléments sur page d'acceuil (index.js), liens vers la page produit selon Id du produit. Création de la page produit.html, produit.js, produit.scss. Liste options à reprendre

--- frontend/view/home/index.js
+++ frontend/view/home/index.js
@@ -1,33 +1,60 @@
 // Affichage et interaction sur page d'acceuil
 
- let url = `http://localhost:3000/api/cameras`;
 
- fetch(url).then( response =>
-    response.json())
-    .then(jsonListCamera => {
-        for (let jsonCamera of jsonListCamera) {
-            let camera = new Camera(jsonCamera);
-            document.querySelector(".articles__container").innerHTML += `<div class="article__card">
-                                                                    <div class="article__card-info">
-                                                                        <img src='${camera.imageUrl}' class="article__card-img"/>                                                                  
-                                                                        <h3 id="name">${camera.name}</h3>
-                                                                        <p id="description">Description : ${camera.description}</p>
-                                                                        <p id="price">${camera.price} €</p>
-                                                                    </div>
-                                                                    <div class="personnalisation">
-                                                                        <div class="lenses">
-                                                                            <label for="lenses">Personnalisez avec nos lentilles :</label>
-                                                                            <select name="lentilles" id"lenses">
-                                                                                <option value="valeur1"selected>${camera.lenses[0]}</option>
-                                                                                <option value="valeur2">${camera.lenses[1]}</option>
-                                                                                <option value="valeur3">${camera.lenses[2]}</option>
-                                                                            </select>
-                                                                        </div>
-                                                                    </div>
-                                                                </div>`;
-        }
-        
-    
-    });
+//fonction "main" qui va s'éxecuter dès le chargement de la page, mais qu'on va remplacer par fonction qui s'appelle toute seule
+//On va récupérer des articles grace à une fonction "getCameras"
+//Après avoir récupérer ces articles, on va devoir les afficher :"displayCamera". avec une boucle "for of" (pour chaque camera de la liste de cameras)
+// Comme je veux retourner le "fetch" je mets ma fonction main en "await", et comme await s'éxécute en asynchone j'y ajoute un async
 
+/*articles__container()
 
+async function articles__container() {
+    const cameras = await getCameras()
+    for (camera of cameras) {
+        displayCamera(camera)
+    }
+}
+*/
+
+(async function() {
+    const cameras = await getCameras()
+    for (camera of cameras) {
+        displayCamera(camera)
+    }
+
+})()
+//On créer la fonction "getCameras"
+// Je fais un fetch pour aller chercher les articles "cameras"
+// Puis j'attribue des fonctions avec ".then" lorsqu'il aura récupérer les données- 1er then me récupére les données et me retourne en json
+// puisque je fais un retour dans un then, j'en fais un second pour récupérer les données du 1er
+
+function getCameras() {
+    return fetch("http://localhost:3000/api/cameras")
+        .then(function(httpBodyResponse) {
+            return httpBodyResponse.json()
+        })
+        .then(function(cameras) {
+            return cameras
+        })
+        .catch(function(erreur) {
+            alert(erreur)
+        })
+}
+
+//On créer notre fonction "displayCamera"
+// Je récupére le template, et créer une constante je le clone et je l'ajoute
+// Je clone et récupére chaque élément que je veux afficher
+// J'en profite pour récupérer chaque id d'article qui me seront nécessaire pour faire le lien vers la page de chaque article
+
+function displayCamera() {
+    const templateElt = document.getElementById("templateArticle")
+    const cloneElt = document.importNode(templateElt.content, true)
+
+    cloneElt.getElementById("imgCamera").src = camera.imageUrl
+    cloneElt.getElementById("name").textContent = camera.name
+    cloneElt.getElementById("description").textContent = camera.description
+    cloneElt.getElementById("price").textContent = camera.price + " €"
+    cloneElt.getElementById("camera__link").href += `?id=${camera._id}`
+
+    document.getElementById("articles__container").appendChild(cloneElt)
+}
---
+++ frontend/view/produit/produit.js
@@ -0,0 +1,48 @@
+// on commence par créer une fonction qui va s'appeler elle même à l'ouverture de la page
+// Cette fonction va récupérer l'id de chaque article dans notre url
+// on va ensuite changer le contenu de chaque article en fonction de l'Id
+
+
+
+(async function() {
+    const cameraId = getCameraId()
+    const camera = await getCamera(cameraId)
+    changeContentCamera(camera)
+    
+})()
+
+// On donne une nouvelle url en fonction de l'id du produit (de l'article)
+function getCameraId() {
+    return new URL(location.href).searchParams.get("id")
+}
+
+// On récupère notre article - Idem au fetch dans la fonction fait en page accueil
+
+function getCamera(cameraId) {
+    return fetch(`http://localhost:3000/api/cameras/${cameraId}`)
+        .then(function(httpBodyResponse) {
+            return httpBodyResponse.json()
+        })
+        .then(function(cameras) {
+            return cameras
+        })
+        .catch(function(erreur) {
+            alert(erreur)
+        })
+}
+
+
+
+
+// j'injecte le contenu selon le contenu de l'Api
+
+function changeContentCamera(camera) {
+   
+
+    document.getElementById("imgCamera").src = camera.imageUrl
+    document.getElementById("name").textContent = camera.name
+    document.getElementById("description").textContent = camera.description
+    document.getElementById("price").textContent = camera.price + " €"
+    document.getElementById("options__lenses").textContent = camera.lenses
+}
+
